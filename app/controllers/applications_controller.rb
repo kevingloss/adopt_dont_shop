ffc88272ed8e0c0ba8252557709ab5a3dfaf868b
Kevin Gloss <kgloss89@gmail.com>
2021-12-11 14:18:25 -0700

    Feat new application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,6 +3,15 @@ class ApplicationsController < ApplicationController
     @application = Application.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    application = Application.create(app_params)
+    # require 'pry'; binding.pry
+    redirect_to "/applications/#{application.id}"
+  end
+
   private
   def app_params
     params.permit(:name, :street, :city, :state, :zip, :description, :status)
