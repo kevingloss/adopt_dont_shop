93ec1b84b54aaf41072d347f1009eefa6bbc192d
Kevin Gloss <kgloss89@gmail.com>
2021-12-11 12:03:51 -0700

    Add enums for status

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,10 +2,6 @@ class Application < ApplicationRecord
   has_many :application_pets
   has_many :pets, through: :application_pets
 
-  validates_presence_of(:name, :street, :city, :state, :zip, :description, :status)
   enum status: { in_progress: 'In Progress', pending: 'Pending', accepted: 'Accepted', rejected: 'Rejected'}
-
-  def status
-    @status = status
-  end
+  validates_presence_of(:name, :street, :city, :state, :zip, :description, :status)
 end
