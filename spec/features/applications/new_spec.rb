6449f523164768b4a798e79e6155b57c06cf3a02
Kevin Gloss <kgloss89@gmail.com>
2021-12-11 18:00:07 -0700

    Test new application flash message

--- spec/features/applications/new_spec.rb
+++ spec/features/applications/new_spec.rb
@@ -24,5 +24,18 @@ RSpec.describe 'new application' do
       application = Application.find_by(name: 'John Doe')
       expect(current_path).to eq("/applications/#{application.id}")
     end
+
+    it 'has a message to fill in any missed fields' do
+      visit "/applications/new"
+
+      fill_in(:name, with: 'John Doe')
+      fill_in(:street, with: '1234 christmas way')
+      fill_in(:city, with: 'Denver')
+      fill_in(:state, with: 'CO')
+      click_button "Submit Application"
+
+      expect(current_path).to eq("/applications/new")
+      expect(page).to have_content("Please fill in all fields.")
+    end
   end
 end
