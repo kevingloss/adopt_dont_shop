73be4655613f889bcd9fd27c305afc986dca74bb
Kevin Gloss <kgloss89@gmail.com>
2021-12-14 16:40:15 -0700

    Move update to proper application_pets_controller

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -2,11 +2,4 @@ class Admin::ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
-
-  def update
-    app_pet = ApplicationPet.find(params[:id])
-    app_pet.update(pet_status: params[:pet_status])
-
-    redirect_to "/admin/applications/#{app_pet.application_id}"
-  end
 end
