4cfefd726f2af41aa775471afe199d375eae122b
Kevin Gloss <kgloss89@gmail.com>
2021-12-14 16:28:30 -0700

    Add column to join table for pet_status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_233843) do
+ActiveRecord::Schema.define(version: 2021_12_14_214839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_12_10_233843) do
   create_table "application_pets", force: :cascade do |t|
     t.bigint "pet_id"
     t.bigint "application_id"
+    t.integer "pet_status", default: 0
     t.index ["application_id"], name: "index_application_pets_on_application_id"
     t.index ["pet_id"], name: "index_application_pets_on_pet_id"
   end
